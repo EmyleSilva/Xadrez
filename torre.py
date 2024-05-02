97b15b9c71408110e840a27c3c0aaffe543ebb92
Maria Clara <mcsjesus.cic@uesc.br>
2024-05-01 23:08:51 -0300

    Arquivo Torre Atualizado

--- torre.py
+++ torre.py
@@ -1,28 +1,27 @@
 import peca
 
 class Torre(peca.Peca):
-   def __init__(self, posicao_origem_x, posicao_origem_y):
-       #Fazer verificação da posicao_de_origem
-       if(posicao_origem_x==0 and posicao_origem_y==7) or (posicao_origem_x==0 or posicao_origem_y == 0) :
-         super().__init__(posicao_origem_x, posicao_origem_y)
-       elif(posicao_origem_x==7 and posicao_origem_y==0) or (posicao_origem_x==7 and posicao_origem_y==7):
-           super().__init__(posicao_origem_x, posicao_origem_y)
-       else:
-           super().__init__(0,0)
+   def __init__(self, posicao_origem_x, posicao_origem_y, cor):
+       super().__init__(posicao_origem_x, posicao_origem_y, cor)
+
 
 
    def validar(self, posicao_x, posicao_y):
-       if (posicao_x != self.posicao_atual_x ) and (posicao_y != self.posicao_atual_y):
-           return False
+       if super().validar(posicao_x, posicao_y):
+           if (posicao_x != self.posicao_atual_x ) and (posicao_y != self.posicao_atual_y):
+             return False
 
-       if((posicao_x== self.posicao_atual_x or posicao_y== self.posicao_atual_y) and
-         (posicao_x!= self.posicao_atual_x or posicao_y!= self.posicao_atual_y)) :
+           if((posicao_x== self.posicao_atual_x or posicao_y== self.posicao_atual_y) and
+             (posicao_x!= self.posicao_atual_x or posicao_y!= self.posicao_atual_y)):
 
-        return True
+            return True
 
 
    def movimentar(self, destino_x, destino_y):
-       if super().validar(destino_x, destino_y):
+
            if self.validar(destino_x, destino_y):
-               self.posicao_atual_x= destino_x
-               self.posicao_atual_y= destino_y
+               self._posicao_atual_x= destino_x
+               self._posicao_atual_y= destino_y
+               return True
+           return False
+

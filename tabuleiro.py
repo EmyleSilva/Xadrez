ce60f1142190b615b3a2197ce9147bdff8912f93
Breno Arouca <banascimento.cic@gmail.com>
2024-05-04 03:08:50 -0300

    Alteração movimento e impressão tabuleiro

--- tabuleiro.py
+++ tabuleiro.py
@@ -25,7 +25,6 @@ class Tabuleiro:
             self._cavalo[-1].indice = indice
             self._matrizPosicao[cavalo.posicao_atual_x][cavalo.posicao_atual_y] = cavalo
 
-
     @property
     def torre(self):
         return self._torre
@@ -39,7 +38,6 @@ class Tabuleiro:
             self._torre.append(torre)
             self._torre[-1].indice = indice
             self._matrizPosicao[torre.posicao_atual_x][torre.posicao_atual_y] = torre
-
 
     @property
     def bispo(self):
@@ -70,93 +68,97 @@ class Tabuleiro:
         if self._bispo[indice].movimentar(destino_x, destino_y):
             if self.validar_movimentacao(destino_x, destino_y, self._bispo[indice]):
                 self._matrizPosicao[destino_x][destino_y] = self._bispo[indice]
-        else:
-            # Caso o movimento de cavalo seja valido mas o tabuleiro não é livre
-            print("Movimento inválido!")
-            self.bispo[indice].desfazer_movimento()
+                self._matrizPosicao[self._bispo[indice].posicao_origem_x][self._bispo[indice].posicao_origem_y] = 0
+                return
+        # Caso o movimento de cavalo seja valido mas o tabuleiro não é livre
+        print("Movimento inválido!")
+        self.bispo[indice].desfazer_movimento()
 
     def movimentar_cavalo(self, destino_x, destino_y, indice):
         if self._cavalo[indice].movimentar(destino_x, destino_y):
             if self.validar_movimentacao(destino_x, destino_y, self._cavalo[indice]):
                 self._matrizPosicao[destino_x][destino_y] = self._cavalo[indice]
                 self._matrizPosicao[self._cavalo[indice].posicao_origem_x][self._cavalo[indice].posicao_origem_y] = 0
-        else:
-            # Caso o movimento de cavalo seja valido mas o tabuleiro não é livre
-            print("Movimento inválido!")
-            self.cavalo[indice].desfazer_movimento()
+                return
+            
+        # Caso o movimento de cavalo seja valido mas o tabuleiro não é livre
+        print("Movimento inválido!")
+        self.cavalo[indice].desfazer_movimento()
 
     def movimentar_torre(self, destino_x, destino_y, indice):
         if self._torre[indice].movimentar(destino_x, destino_y):
             if self.validar_movimentacao(destino_x, destino_y, self._torre[indice]):
                 self._matrizPosicao[destino_x][destino_y] = self._torre[indice]
                 self._matrizPosicao[self._torre[indice].posicao_origem_x][self._torre[indice].posicao_origem_y] = 0
-        else:
+                return
             # Caso o movimento de cavalo seja valido mas o tabuleiro não é livre
-            print("Movimento inválido!")
-            self.torre[indice].desfazer_movimento()
+        print("Movimento inválido!")
+        self.torre[indice].desfazer_movimento()
+
+    def verificaTipo(self, elemento):
+        if elemento == 0:
+            return " \U000025FD "
+        else:
+            if isinstance(elemento, cavalo.Cavalo):
+                if elemento.cor == "Branco":
+                    return " \U00002658  "
+                else:
+                    return " \U0000265E  "
+            elif isinstance(elemento, bispo.Bispo):
+                if elemento.cor == "Branco":
+                    return " \U00002657  "
+                else:
+                    return " \U0000265D  "
+            elif isinstance(elemento, torre.Torre): 
+                if elemento.cor == "Branco":
+                    return " \U00002656  "
+                else:
+                    return " \U0000265C  "
+
+    def capturados(self, cor):
+        for i in range(len(self._bispo)):
+            if self._bispo[i].estado == False and self._bispo[i].cor == cor:
+                print(f"{self.verificaTipo(self._bispo[i])}", end="")
+        for i in range(len(self._torre)):
+            if self._torre[i].estado == False and self._torre[i].cor == cor:
+                print(f"{self.verificaTipo(self._torre[i])}", end="")
+        for i in range(len(self._cavalo)):
+            if self._cavalo[i].estado == False and self._cavalo[i].cor == cor:
+                print(f"{self.verificaTipo(self._cavalo[i])}", end="")
+        print()
 
     def imprimir_tabuleiro(self):
         i = 0
-        j = 0
+        self.capturados("Preto")
         for linha in self._matrizPosicao:
-            j = 0
-            k = 0
             i += 1
             print(f"{i}", end=" | ")
-            for elemento in linha:
-                if elemento == 0:
-                    print("\U000025FD", end=" ")
-                elif(elemento != 0 and elemento.estado == False):
-                    print("\U000025FD", end=" | ")
-                
-                else:
-                    if isinstance(elemento, cavalo.Cavalo):
-                        if elemento.cor == "Branco":
-                            print("\U00002658", end=" ")
-                        else:
-                            print("\U0000265E", end=" ")
-                    elif isinstance(elemento, bispo.Bispo):
-                        if elemento.cor == "Branco":
-                            print("\U00002657", end=" |")
-                        else:
-                            print("\U0000265D", end=" | ")
-                    elif isinstance(elemento, torre.Torre): 
-                        if elemento.cor == "Branco":
-                            print("\U00002656", end=" |")
-                        else:
-                            print("\U0000265C", end=" | ")
-                k+=1
-            j+=1
+            for elemento in linha: 
+                print(f"{self.verificaTipo(elemento)}", end=" |")
             print()
+        
+        self.capturados("Branco")
+        print()
 
 
 b = bispo.Bispo(0, 0, "Branco")
-b2 = bispo.Bispo(1, 1, "Branco")
+b2 = bispo.Bispo(2, 4, "Branco")
 b3 = bispo.Bispo(0, 3, "Branco")
-b4 = bispo.Bispo(2, 4, "Preto")
+b4 = bispo.Bispo(5, 5, "Preto")
 t = Tabuleiro()
-
-t.imprimir_tabuleiro()
 
 t.bispo = b
 t.bispo = b2
-t.imprimir_tabuleiro()
-print("")
+t.bispo = b3
+t.bispo = b4
 
 t1 = torre.Torre(1,2,"Branco")
 t.torre = t1
 t2 = torre.Torre(2,3,"Preto")
 t.torre = t2
-t.imprimir_tabuleiro()
+
 c = cavalo.Cavalo(3,2, "Preto")
 t.cavalo = c
 
 t.movimentar_torre(2, 6, t2.indice)
-t.movimentar_cavalo(3,1, c.indice)
-
-t.bispo = b
 t.imprimir_tabuleiro()
-print("Posicao atual bispo 1: ", b.posicao_atual_x, b.posicao_atual_y)
-t.movimentar_bispo(1, 1, b.indice)
-print("Posicao atual bispo 1: ", b.posicao_atual_x, b.posicao_atual_y)
-print(b2.estado)
